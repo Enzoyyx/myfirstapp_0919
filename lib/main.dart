cdc8d7710fd21b415e68badf18d39554c3945274
Enzoyyx <yuxiang31740922@gmail.com>
2022-11-14 16:59:36 +0800

    408601000_image

--- lib/main.dart
+++ lib/main.dart
@@ -1,81 +1,128 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-import 'package:photo_view/photo_view.dart';
-void main() => runApp(const SnackBarDemo());
 
-class SnackBarDemo extends StatelessWidget {
-  const SnackBarDemo({super.key});
+void main() => runApp(const MyAPP());
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'SnackBar Demo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('SnackBar Demo'),
-        ),
-        body: const SnackBarPage(),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blueGrey,
       ),
+      home: const MyHomePage(title: '期中實做作業'),
     );
   }
 }
 
-class ImageBrowser extends StatefulWidget{
-  final GlobalKey<_ImageBrowserState> _key;
-  List<String> _images;
-  int _imageIndex;
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
 
-  _ImageBrowser(this._key, this._images): super (key: _key) {
-  _imageIndex = 0;
-  }
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
 
   @override
-  State<StatefulWidget>  createState() => _ImageBrowserState();
-
-  previousImage () => _key. currentState .previousImage();
-  nextImage() => _key .currentState.nextImage();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _ImageBrowserState extends State<_ImageBrowser> {
-  @override
-  Widget build (BuildContext context) {
-    var img = PhotoView(
-        imageProvider: AssetImage (widget._images[widget ._imageIndex]),
-        minscale: PhotoViewComputedScale.contained * 0.6,
-        maxScale: PhotoViewComputedScale.covered,
-        enableRotation: true,
-        backgroundDecoration: BoxDecoration(
-          color:Colors.white,
-        ));
-    return img;
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
 
+  int counter = 0;
+  final imglist = ['asset/image1.jpg','asset/image2.jpg','asset/image3.jpg','asset/image4.jpg'];
+  final imgname = ['image1','image2','image3','image4'];
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
   }
-}
-
-class SnackBarPage extends StatelessWidget {
-  const SnackBarPage({super.key});
-
+  tmpFunction() {
+    Container(
+      child: Image(image: AssetImage(imglist[counter]),
+      ),
+    );
+  }
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: ElevatedButton(
-        onPressed: () {
-          final snackBar = SnackBar(
-            content: const Text('Yay! A SnackBar!'),
-            action: SnackBarAction(
-              label: 'Undo',
-              onPressed: () {
-                // Some code to undo the change.
-              },
-            ),
-          );
-
-          // Find the ScaffoldMessenger in the widget tree
-          // and use it to show a SnackBar.
-          ScaffoldMessenger.of(context).showSnackBar(snackBar);
-        },
-        child: const Text('Show SnackBar'),
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
       ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  child: Image(image: AssetImage(imglist[counter])),
+                  width:300,
+                  height:300,
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                    padding: EdgeInsets.only(right: 50),
+                    child: ElevatedButton(onPressed: (){
+                      counter--;
+                      if(counter<0){
+                        counter = imglist.length-1;
+                      }
+                      setState(() {counter;});
+                      Fluttertoast.showToast(msg: 'previous');
+                    }, child: Text('<',style: TextStyle(fontSize: 28),))),
+                Container(
+                    padding: EdgeInsets.only(left: 50),
+                    child: ElevatedButton(onPressed: (){
+                      counter++;
+                      if(counter>imglist.length-1){
+                        counter = 0;
+                      }
+                      setState(() {counter;});
+                      Fluttertoast.showToast(msg: 'next');
+                    }, child: Text('>',style: TextStyle(fontSize: 28),))),
+              ],
+            )
+          ],
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
